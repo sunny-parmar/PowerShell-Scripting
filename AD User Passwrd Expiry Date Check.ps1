b78eec6bc1fe6b795430a7ece32607e4cb2f742f
Sunny Parmar <84026786+sunny-parmar@users.noreply.github.com>
2021-12-28 09:39:58 +0530

    Add files via upload

---
+++ AD User Passwrd Expiry Date Check.ps1
@@ -0,0 +1,1 @@
+Get-ADUser -filter {Enabled -eq $True -and PasswordNeverExpires -eq $False} –Properties "DisplayName", "msDS-UserPasswordExpiryTimeComputed" | Select-Object -Property "Displayname",@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}}
